05b03a7143672dd14d31b882337b089e8f2e47dd
Ian Hickson <ian@hixie.ch>
2018-09-04 19:54:55 -0700

    reorganize files

--- lib/all.dart
+++ lib/all.dart
@@ -1,18 +1,18 @@
 library home_automation_tools;
 
-export 'src/air_quality.dart';
-export 'src/cloudbit.dart';
-export 'src/cloudbit_cloud.dart';
-export 'src/cloudbit_local.dart';
 export 'src/common.dart';
+export 'src/devices/air_quality.dart';
+export 'src/devices/cloudbit.dart';
+export 'src/devices/cloudbit_cloud.dart';
+export 'src/devices/cloudbit_local.dart';
+export 'src/devices/remy.dart';
+export 'src/devices/sunpower.dart';
+export 'src/devices/tts.dart';
+export 'src/devices/tv.dart';
+export 'src/devices/wake_on_lan.dart';
 export 'src/hash_codes.dart';
 export 'src/json.dart';
-export 'src/remy.dart';
-export 'src/sunpower.dart';
 export 'src/temperature.dart';
-export 'src/tts.dart';
-export 'src/tv.dart';
-export 'src/wake_on_lan.dart';
 export 'src/watch_stream.dart';
 
 // ## Local 1-wire temperature sensor
