b22a8285d03c59df9984abd3eab3e42e425af669
treeplate <51892395+treeplate@users.noreply.github.com>
2021-09-01 15:50:50 -0700

    add docs for packetbuffer

--- lib/src/packet_buffer.dart
+++ lib/src/packet_buffer.dart
@@ -6,25 +6,43 @@ class PacketBuffer {
 
   final Queue<Uint8List> _buffer = Queue<Uint8List>();
 
-  int _start = 0;
-  int _cursor = 0;
-  int _length = 0;
+  int _start = 0; // index of last checkpoint
+  int _cursor = 0; // index of end of last read
+  int _length = 0; // total bytes allocated
 
+  /// Adds [data] to the end of the buffer.
+  /// Increments [available] by the length of [data].
   void add(Uint8List data) {
     _buffer.add(data);
     _length += data.length;
-    assert(_buffer.fold<int>(0, (int current, Uint8List next) => current + next.length) == _length);
+    assert(
+      _buffer.fold<int>(
+            0,
+            (int current, Uint8List next) => current + next.length,
+          ) ==
+          _length,
+    );
   }
 
+  /// Returns the number of unread bytes.
   int get available {
-    assert(_buffer.fold<int>(0, (int current, Uint8List next) => current + next.length) == _length);
+    assert(
+      _buffer.fold<int>(
+            0,
+            (int current, Uint8List next) => current + next.length,
+          ) ==
+          _length,
+    );
     return _length - _cursor;
   }
 
+  /// Marks all bytes as unread.
   void rewind() {
     _cursor = _start;
   }
 
+  /// Reads the first 8 unread bytes, and returns them as an integer.
+  /// Marks those bytes as read and decrements [available] by 8.
   int readInt64() {
     assert(available >= 8); // contract
     assert(_start < _buffer.first.length); // invariant
@@ -50,12 +68,17 @@ class PacketBuffer {
     throw StateError('Unreachable.');
   }
 
+  /// Reads the first [length] unread bytes, and returns them as an [Uint8List].
+  /// Marks those bytes as read and decrements [available] by [length].
   Uint8List readUint8List(int length) {
     assert(length <= available); // contract
     assert(_start < _buffer.first.length); // invariant
+    if (length == 0) {
+      return Uint8List(0);
+    }
     int packetOffset = _cursor;
     for (Uint8List packet in _buffer) {
-      if (packetOffset > packet.length) {
+      if (packetOffset >= packet.length) {
         packetOffset -= packet.length;
         continue;
       }
@@ -70,19 +93,28 @@ class PacketBuffer {
         packetOffset = _cursor;
       }
       _cursor += length;
-      return bytes.buffer.asUint8List(bytes.offsetInBytes + packetOffset, length);
+      return bytes.buffer
+          .asUint8List(bytes.offsetInBytes + packetOffset, length);
     }
     throw StateError('Unreachable.');
   }
 
+  // Shouldn't this be private?
   void flatten() {
-    Uint8List bytes = Uint8List(available);
+    Uint8List bytes = Uint8List(_length - _start);
     _length = bytes.length;
     _cursor -= _start;
     int index = 0;
     for (Uint8List packet in _buffer) {
-      if (_start > 0) { 
-        bytes.setRange(index, index + (packet.length - _start), packet.buffer.asUint8List(packet.offsetInBytes + _start, packet.length - _start));
+      if (_start > 0) {
+        bytes.setRange(
+          index,
+          index + (packet.length - _start),
+          packet.buffer.asUint8List(
+            packet.offsetInBytes + _start,
+            packet.length - _start,
+          ),
+        );
         index += packet.length - _start;
         _start = 0;
       } else {
@@ -96,6 +128,7 @@ class PacketBuffer {
     assert(_cursor <= _buffer.first.length);
   }
 
+  /// Forgets all bytes marked as read.
   void checkpoint() {
     _start = _cursor;
     while (_buffer.isNotEmpty && _buffer.first.length <= _start) {
